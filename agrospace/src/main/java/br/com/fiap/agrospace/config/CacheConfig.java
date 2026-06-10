5fb67e526f469ea5c382d6fac9c37f43ac535acd
EmilyMacedo278 <emily27289@Gmail.com>
2026-06-09 21:12:57 -0300

    Ultimo commit

---
+++ agrospace/src/main/java/br/com/fiap/agrospace/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package br.com.fiap.agrospace.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager(
+                "fazendas",
+                "fazenda",
+                "areasAgricolas",
+                "areaAgricola",
+                "satelites",
+                "satelite",
+                "leiturasAmbientais",
+                "leituraAmbiental"
+        );
+    }
+}
